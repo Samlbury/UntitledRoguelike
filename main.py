9130944faf944cbc5238fe43d06a38c1993b03f9
Samlbury <sammysalisbury94@gmail.com>
2024-05-13 19:41:11 -0400

    added basic player and movement

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
-#!/user/bin/env python
+#!/usr/bin/env python
 
+import attrs
 import tcod
+from tcod.console import Console
+from tcod.event import KeySym
+
+@attrs.define(eq=False)
+class ExampleState:
+    player_x: int
+    player_y: int
+
+    def on_draw(self, console: Console) -> None:
+        console.print(self.player_x, self.player_y, "@")
+
+    def on_event(self, event: tcod.event.Event) -> None:
+        match event:
+            case tcod.event.Quit():
+                raise SystemExit()
+            case tcod.event.KeyDown(sym=KeySym.LEFT):
+                self.player_x -= 1
+            case tcod.event.KeyDown(sym=KeySym.RIGHT):
+                self.player_x += 1
+            case tcod.event.KeyDown(sym=KeySym.UP):
+                self.player_y -= 1
+            case tcod.event.KeyDown(sym=KeySym.DOWN):
+                self.player_y += 1
 
 def main() -> None:
     tileset = tcod.tileset.load_tilesheet("data/Alloy_curses_12x12.png", columns=16, rows=16, charmap=tcod.tileset.CHARMAP_CP437)
     tcod.tileset.procedural_block_elements(tileset=tileset)
     
-    console = tcod.console.Console(80, 50)
-    console.print(0, 0, "Hello World!") # Test print
+    console = Console(80, 50)
+
+    state = ExampleState(player_x=console.width // 2, player_y=console.height //2)
     
     with tcod.context.new(console=console, tileset=tileset) as context:
         while True:
+            console.clear()
+            state.on_draw(console)
             context.present(console)
             for event in tcod.event.wait():
-                if isinstance(event, tcod.event.Quit):
-                    raise SystemExit()
+                state.on_event(event)
 
 if __name__ == "__main__":
     main()
